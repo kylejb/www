aed14e791f2040bc1c3379464cc79971d0e0eb4d
kylejb <47502769+kylejb@users.noreply.github.com>
2020-10-19 11:35:37 -0400

    Linked 'scss' stylesheet to 'index.js', and added fontawesome module

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './libs/fontawesome/all.min.js';
+import './styles/base.scss';
 import App from './App';
-import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
