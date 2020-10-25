d08e07ac807a383a9b2086c2bdfb098e4b4e3895
kylejb <47502769+kylejb@users.noreply.github.com>
2020-10-25 19:36:16 -0400

    Refactored to useContext and added state to maintain awareness of currentY onScroll

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,47 +1,65 @@
-import React, { useContext, useRef, useState } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import { ThemeContext } from '../../ThemeContext';
 import { toElement as scrollToElement } from '../../utils/scroll';
-
 import './style.scss';
 
-const Nav = (props) => {
+
+const Nav = ( props ) => {
   const theme = useContext(ThemeContext);
-  let nav = useRef();
+  const { currentTheme: { colorPrimary, bgPrimary, navAlpha }, switchTheme } = theme;
+
   const [isSticky, setIsSticky] = useState(false);
+  const [goingUp, setGoingUp] = useState(false);
+  
+  const prevScrollY = useRef(0);
+  const nav = useRef();
 
-  // componentDidMount() {
-  //   window.addEventListener('scroll', handleScroll);
-  // }
+  const stickyClass = isSticky ? 'sticky' : '';
+  const stickyStyles = isSticky
+    ? { backgroundColor: navAlpha, color: colorPrimary }
+    : { backgroundColor: bgPrimary, color: colorPrimary };
 
-  // componentWillUnmount() {
-  //   window.removeEventListener('scroll', handleScroll);
-  // }
-
-  const handleScroll = () => {
-    if (window.pageYOffset > nav.offsetTop) {
-      setIsSticky(true);
-    } else {
-      setIsSticky(false);
-    }
-  }
 
   const scrollToPage = (pageSelector) => {
     const nextPage = document.querySelector(pageSelector);
     scrollToElement(nextPage);
   }
-  const { currentTheme: { colorPrimary, bgPrimary, navAlpha }, switchTheme } = theme;
+
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      if (prevScrollY.current < currentScrollY && goingUp) {
+        setGoingUp(false);
+      }
+      if (prevScrollY.current > currentScrollY && !goingUp) {
+        setGoingUp(true);
+      }
+      const domRect = nav.current.getBoundingClientRect()
+      if (currentScrollY > domRect.height) {
+        setIsSticky(true);
+      } else {
+        setIsSticky(false);
+      }
+      prevScrollY.current = currentScrollY;
+      //! console.log(goingUp, currentScrollY);
+    };
+    
+    window.addEventListener("scroll", handleScroll, { passive: true });
+    
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [goingUp, nav]);
   
-  const stickyClass = isSticky ? 'sticky' : '';
-  const stickyStyles = isSticky
-    ? { backgroundColor: navAlpha, color: colorPrimary }
-    : { backgroundColor: bgPrimary, color: colorPrimary };
+  //! if (nav.current) {
+  //!   console.log("nav render window offset ", nav.current.getBoundingClientRect().height)
+  //! } 
+
+
   return (
     <nav
+      ref={nav}
       className={stickyClass}
-      // ref={(elem) => {
-      //   this.nav = elem;
-      // }}
       style={stickyStyles}
     >
       <div className="magic-wand bounce-xy" onClick={(e) => switchTheme()}>
@@ -79,96 +97,3 @@ Nav.propType = {
 };
 
 export default Nav;
-
-
-
-// class Nav extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.handleScroll = this.handleScroll.bind(this);
-//     this.state = {
-//       isSticky: false
-//     };
-//   }
-
-//   componentDidMount() {
-//     window.addEventListener('scroll', this.handleScroll);
-//   }
-
-//   componentWillUnmount() {
-//     window.removeEventListener('scroll', this.handleScroll);
-//   }
-
-//   handleScroll() {
-//     if (window.pageYOffset > this.nav.offsetTop) {
-//       this.setState({
-//         isSticky: true
-//       });
-//     } else {
-//       this.setState({
-//         isSticky: false
-//       });
-//     }
-//   }
-
-//   scrollToPage(pageSelector) {
-//     const nextPage = document.querySelector(pageSelector);
-//     scrollToElement(nextPage);
-//   }
-
-//   render() {
-//     this.nav = 123
-//     console.log("Class Nav ", this.nav)
-//     const {
-//       theme: { colorPrimary, bgPrimary, navAlpha },
-//       switchTheme
-//     } = this.context;
-
-//     const stickyClass = this.isSticky ? 'sticky' : '';
-//     const stickyStyles = this.isSticky
-//       ? { backgroundColor: navAlpha, color: colorPrimary }
-//       : { backgroundColor: bgPrimary, color: colorPrimary };
-//     return (
-//       <nav
-//         className={stickyClass}
-//         ref={(elem) => {
-//           this.nav = elem;
-//         }}
-//         style={stickyStyles}
-//       >
-//         <div className="magic-wand bounce-xy" onClick={(e) => switchTheme()}>
-//           <button className="fas fa-magic fa-lg" href="#" />
-//           <div className="magic-text">Color Me</div>
-//         </div>
-//         <style jsx="true">
-//           {`
-//             .menu__item:hover {
-//               border-bottom: 2px solid ${colorPrimary};
-//             }
-//           `}
-//         </style>
-//         <div className="menu">
-//           <div
-//             className="menu__item active"
-//             onClick={(e) => this.scrollToPage('.intro-wrapper')}
-//           >
-//             About
-//           </div>
-//           <div
-//             className="menu__item"
-//             onClick={(e) => this.scrollToPage('.portfolio-page')}
-//           >
-//             Portfolio
-//           </div>
-//         </div>
-//       </nav>
-//     );
-//   }
-// }
-
-// Nav.contextTypes = {
-//   theme: PropTypes.any,
-//   switchTheme: PropTypes.func
-// };
-
-// export default Nav;
--- src/components/ScrollToNext/ScrollToNext.jsx
+++ src/components/ScrollToNext/ScrollToNext.jsx
@@ -1,22 +1,23 @@
 import React, { useContext } from 'react';
-import { ThemeContext } from '../../ThemeContext';
 import PropTypes from 'prop-types';
+import { ThemeContext } from '../../ThemeContext';
 import { toElement as scrollToElement } from '../../utils/scroll';
-
 import './style.scss';
+
 
 const ScrollToNext = (props) => {
   const theme = useContext(ThemeContext);
+  const { currentTheme: { colorPrimary } } = theme;
 
   const scrollToNext = () => {
-    const { pageSelector } = this.props;
+    const { pageSelector } = props;
     const nextPage = document.querySelector(pageSelector);
     scrollToElement(nextPage);
   }
   
-  const { currentTheme: { colorPrimary } } = theme;
+
   return (
-    <div className="scroll-to-next" onClick={(e) => this.scrollToNext()}>
+    <div className="scroll-to-next" onClick={(e) => scrollToNext()}>
       <div className="arrow bounce" style={{ color: colorPrimary }}>
         <div className="scroll-text">Click Me</div>
         <button className="fas fa-chevron-down fa-2x" href="#" />
