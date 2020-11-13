261636552f84a6fd34763780030578c6b7d0408c
kylejb <47502769+kylejb@users.noreply.github.com>
2020-11-13 01:21:33 -0500

    Added twitter icon

--- src/components/SocialIcons/SocialIcons.jsx
+++ src/components/SocialIcons/SocialIcons.jsx
@@ -12,6 +12,7 @@ const SocialIcons = () => {
   return (
     <div className="social-icons animate-icons">
         <a target="_blank" rel="noopener noreferrer" href="https://github.com/kylejb" style={ { color: colorPrimary } }><i className="fab fa-github"></i></a>
+        <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/kyleburda" style={ { color: colorPrimary } }><i className="fab fa-twitter"></i></a>
         <a target="_blank" rel="noopener noreferrer" href="https://stackoverflow.com/story/kylejb" style={ { color: colorPrimary } }><i className="fab fa-stack-overflow"></i></a>
         <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/kyleburda" style={ { color: colorPrimary } }><i className="fab fa-linkedin"></i></a>
         <a className="devto" target="_blank" rel="noopener noreferrer" href="https://dev.to/kylejb">
