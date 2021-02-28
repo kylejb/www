6b5fb254c21f128673f6e16282150ab46ee74838
kylejb <47502769+kylejb@users.noreply.github.com>
2021-02-28 10:26:21 -0500

    Added Roboto font-face and removed scrollbar

--- src/styles/globalStyles.jsx
+++ src/styles/globalStyles.jsx
@@ -1,6 +1,40 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
+
+  /* Typography */
+  @font-face {
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 100;
+    src: local('Roboto Thin'), local('Roboto-Thin'),
+    url('/fonts/roboto-100.woff2') format('woff2');
+  }
+
+  @font-face {
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 300;
+    src: local('Roboto Light'), local('Roboto-Light'),
+    url('/fonts/roboto-300.woff2') format('woff2');
+  }
+
+  @font-face {
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    src: local('Roboto'), local('Roboto-Regular'),
+    url('/fonts/roboto-400.woff2') format('woff2');
+  }
+
+  @font-face {
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 900;
+    src: local('Roboto Black'), local('Roboto-Black'),
+    url('/fonts/roboto-900.woff2') format('woff2');
+  }
+
   html {
     box-sizing: border-box;
     font-size: 16px;
@@ -28,7 +62,10 @@ const GlobalStyle = createGlobalStyle`
     font-weight: 300;
   }
 
-  h1,
+  h1 {
+    font-weight: 900;
+  }
+
   h2,
   h3,
   h4,
@@ -50,6 +87,10 @@ const GlobalStyle = createGlobalStyle`
   a {
     color: inherit;
     text-decoration: none;
+  }
+
+  ::-webkit-scrollbar {
+	  display: none;
   }
 `;
 
