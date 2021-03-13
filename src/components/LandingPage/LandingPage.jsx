aa3de363ca54a8a38bc4f8f4b2f9435816a3092c
kylejb <47502769+kylejb@users.noreply.github.com>
2021-03-13 12:17:54 -0500

    Removed scrollTo action and reformatted spacing

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,28 +1,26 @@
 import { useThemeContext } from '../../contexts/theme/ThemeContext';
 import SocialIcons from '../SocialIcons/index';
-import ScrollToNext from '../ScrollToNext/index';
 import { Container } from './styledComponents';
 
 
 const LandingPage = () => {
-  const theme = useThemeContext();
-  const { currentTheme: { bgPrimary, colorPrimary } } = theme;
+    const theme = useThemeContext();
+    const { currentTheme: { bgPrimary, colorPrimary } } = theme;
 
 
-  return (
-    <Container style={{ backgroundColor: bgPrimary }} className="landing-page">
-      <main style={{ color: colorPrimary }}>
-        <div className="intro-wrapper">
-          <div className="intro-name">Hello, I'm Kyle!</div>
-          <div className="tagline">
-            Full Stack Dev | Open-Source Enthusiast | Blogger | Avid Surfer
-          </div>
-          <SocialIcons />
-        </div>
-      </main>
-      <ScrollToNext pageSelector=".portfolio-page" />
-    </Container>
-  );
+    return (
+        <Container style={{ backgroundColor: bgPrimary }} className="landing-page">
+            <main style={{ color: colorPrimary }}>
+                <div className="intro-wrapper">
+                    <div className="intro-name">Hello, I'm Kyle!</div>
+                    <div className="tagline">
+                        Full Stack Dev | Open-Source Enthusiast | Blogger | Avid Surfer
+                    </div>
+                    <SocialIcons />
+                </div>
+            </main>
+        </Container>
+    );
 };
 
 export default LandingPage;
--- src/pages/PortfolioPage/PortfolioPage.jsx
+++ src/pages/PortfolioPage/PortfolioPage.jsx
@@ -1,35 +1,33 @@
 import PortfolioCard from '../../components/PortfolioCard';
-import ScrollToPrevious from '../../components/ScrollToPrevious';
 import { useThemeContext } from '../../contexts/theme/ThemeContext';
 import { projects } from './projects';
 import {
-  GridContainer,
-  GridRow,
-  GridColumn
+    GridContainer,
+    GridRow,
+    GridColumn
 } from '../../components/PortfolioCard/expStyledComponent';
 
 
 const PortfolioPage = () => {
-  const theme = useThemeContext();
-  const { currentTheme: {colorPrimary, colorAlternate, textAlternate, bgPrimary } } = theme;
+    const theme = useThemeContext();
+    const { currentTheme: { colorPrimary, colorAlternate, textAlternate, bgPrimary } } = theme;
 
-  const renderProj = () => projects.map((project) => (
-    <GridColumn key={project.id} sm='6' lg='4'>
-      <PortfolioCard className={project.title} id={project.id} project={project} key={project.id} />
-    </GridColumn>
-  ));
+    const renderProj = () => projects.map((project) => (
+        <GridColumn key={project.id} sm='6' lg='4'>
+            <PortfolioCard className={project.title} id={project.id} project={project} key={project.id} />
+        </GridColumn>
+    ));
 
-  return (
-    <div className="portfolio-page" style={{ display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: bgPrimary }}>
-      <GridContainer>
-        <h1 style={{ padding: "1rem 0", fontSize: "3rem", textAlign: "center", color: colorPrimary }}>Portfolio</h1>
-        <GridRow breakpoints={[700]}>
-          {renderProj()}
-        </GridRow>
-        <ScrollToPrevious pageSelector=".landing-page" />
-      </GridContainer>
-    </div>
-  );
+    return (
+        <div className="portfolio-page" style={{ display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: bgPrimary }}>
+            <GridContainer>
+                <h1 style={{ padding: "1rem 0", fontSize: "3rem", textAlign: "center", color: colorPrimary }}>Portfolio</h1>
+                <GridRow breakpoints={[700]}>
+                    {renderProj()}
+                </GridRow>
+            </GridContainer>
+        </div>
+    );
 };
 
 export default PortfolioPage;
