e9d5bb32e738c62454fb08606272d19742e7d7b6
kylejb <47502769+kylejb@users.noreply.github.com>
2021-03-14 15:30:00 -0400

    Created global css variables file with breakpoints

---
+++ src/styles/_variables.js
@@ -0,0 +1,9 @@
+export const breakpoints = {
+    xsmall: '0px', // mobile-m max=375px
+    small: '376px', // mobile-l max=425px
+    medium: '426px', // tablet max=768px
+    large: '769px', // laptop-m max=1024px => ipad pro falls under this
+    xlarge: '1025px', // laptop-l max=1440px
+    xxlarge: '1440px', // 4k max=2560px
+    xxxlarge: '2560px'
+};
