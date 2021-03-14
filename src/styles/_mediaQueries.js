055dc4838caa43825471299f45ad6f09da5c1426
kylejb <47502769+kylejb@users.noreply.github.com>
2021-03-14 16:11:09 -0400

    Added mediaQuery for max-width and appended 'px'

--- src/styles/_mediaQueries.js
+++ src/styles/_mediaQueries.js
@@ -2,11 +2,19 @@ import { mediaQuery } from '../utils/mediaQuery';
 import { breakpoints } from './_variables';
 
 export const media = {
-    mobileM: mediaQuery`(min-width: ${breakpoints.xsmall})`,
-    mobileL: mediaQuery`(min-width: ${breakpoints.small})`,
-    tablet: mediaQuery`(min-width: ${breakpoints.medium})`,
-    laptopM: mediaQuery`(min-width: ${breakpoints.large})`,
-    laptopL: mediaQuery`(min-width: ${breakpoints.xlarge})`,
-    desktopM: mediaQuery`(min-width: ${breakpoints.xxlarge})`,
-    desktopL: mediaQuery`(min-width: ${breakpoints.xxxlarge})`
+    mobileM: mediaQuery`(min-width: ${breakpoints.xsmall}px)`,
+    mobileL: mediaQuery`(min-width: ${breakpoints.small}px)`,
+    tablet: mediaQuery`(min-width: ${breakpoints.medium}px)`,
+    laptopM: mediaQuery`(min-width: ${breakpoints.large}px)`,
+    laptopL: mediaQuery`(min-width: ${breakpoints.xlarge}px)`,
+    desktopM: mediaQuery`(min-width: ${breakpoints.xxlarge}px)`,
+    desktopL: mediaQuery`(min-width: ${breakpoints.xxxlarge}px)`
 };
+
+export const maxDownMediaBreakpoints = {
+    mobileL: mediaQuery`(max-width: ${breakpoints.medium - 1}px)`,
+    tablet: mediaQuery`(max-width: ${breakpoints.large - 1}px)`,
+    laptopM: mediaQuery`(max-width: ${breakpoints.xlarge - 1}px)`,
+    laptopL: mediaQuery`(max-width: ${breakpoints.xxlarge - 1}px)`,
+    desktopM: mediaQuery`(max-width: ${breakpoints.xxxlarge - 1}px)`
+}
