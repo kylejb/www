ed620f536ce73593901d6b915bddf0f295c85d97
kylejb <47502769+kylejb@users.noreply.github.com>
2021-03-16 03:08:41 -0400

    Fixed to reference values in '_variables.js'

--- src/styles/_keyframes.js
+++ src/styles/_keyframes.js
@@ -1,26 +1,26 @@
 import { keyframes } from 'styled-components';
-
+import { degreeVal, perspectiveVal } from './_variables';
 
 export const back_flip_1 = keyframes`
   0% {transform: rotateY(0deg); z-index: 1}
-  50% {transform: rotateY(${degree}) perspective(${perspective}); z-index: 1}
-  100% {transform: rotateY(${degree}); z-index: -1}
+  50% {transform: rotateY(${degreeVal}) perspective(${perspectiveVal}); z-index: 1}
+  100% {transform: rotateY(${degreeVal}); z-index: -1}
 `;
 
 export const back_flip_2 = keyframes`
-  0% {transform: rotateY(-${degree}); z-index: -1}
-  50% {transform: rotateY(-${degree}) perspective(${perspective}); z-index: -1}
+  0% {transform: rotateY(-${degreeVal}); z-index: -1}
+  50% {transform: rotateY(-${degreeVal}) perspective(${perspectiveVal}); z-index: -1}
   100% {transform: rotateY(0deg); z-index: 1}
 `;
 
 export const front_flip_1 = keyframes`
-  0% {transform: rotateY(-${degree}); z-index: -1}
-  50% {transform: rotateY(-${degree}) perspective(${perspective}); z-index: -1}
+  0% {transform: rotateY(-${degreeVal}); z-index: -1}
+  50% {transform: rotateY(-${degreeVal}) perspective(${perspectiveVal}); z-index: -1}
   100% {transform: rotateY(0deg); z-index: 1}
 `;
 
 export const front_flip_2 = keyframes`
   0% {transform: rotateY(0deg); z-index: 1}
-  50% {transform: rotateY(${degree}) perspective(${perspective}); z-index: 1}
-  100% {transform: rotateY(${degree}); z-index: -1}
+  50% {transform: rotateY(${degreeVal}) perspective(${perspectiveVal}); z-index: 1}
+  100% {transform: rotateY(${degreeVal}); z-index: -1}
 `;
