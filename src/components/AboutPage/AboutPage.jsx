388bab2e3915892fbedaca69342ff03090cccc57
kylejb <47502769+kylejb@users.noreply.github.com>
2021-04-22 11:56:05 -0400

    Adding preliminary content [WIP]

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -10,9 +10,28 @@ const AboutPage = () => {
         <Container style={{ backgroundColor: bgPrimary }} className="about-page">
             <main style={{ color: colorPrimary }}>
                 <div className="about-wrapper">
-                    <div className="about-header">About Me</div>
+                    <div className="about-header">$ whoami</div>
+                    {/*
+                        // TODO - consider adding terminal animation with 'whoami' being entered that returns the div below
+                    */}
                     <div className="about-content">
-                        <p>This is my about me section.</p>
+                        <p>I am driven by a deep desire to utilize technology to make a positive impact.</p>
+
+                        <h4>Skills</h4>
+                        <h6>Languages</h6>
+                        <p>
+                            Python, Ruby, JavaScript, TypeScript, SQL, HTML, CSS
+                        </p>
+
+                        <h6>Libraries and Frameworks</h6>
+                        <p>
+                            React, Redux, Node, Express, Ruby on Rails, FastAPI, Django, Flask, Jest, Selenium
+                        </p>
+
+                        <h6>Databases and Tools</h6>
+                        <p>
+                            AWS, git, Docker, Postman, Heroku, Travis-CI, PostgreSQL, SQLite3
+                        </p>
                     </div>
                 </div>
             </main>
