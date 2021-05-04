28b0caee705e134b141c5e318f4d86b06628d22a
kylejb <47502769+kylejb@users.noreply.github.com>
2021-05-03 23:56:14 -0400

    Wired up gif with next/image to test layout [WIP]

--- src/components/PortfolioCard/Card/front.component.jsx
+++ src/components/PortfolioCard/Card/front.component.jsx
@@ -1,48 +1,41 @@
-import { CardDescription, CardTitle, CardIcon, CardLink, CardLinkWrapper } from '../styledComponent';
+import Image from 'next/image';
+import {
+    CardDescription,
+    CardTitle,
+    CardIcon,
+    CardLink,
+    CardLinkWrapper
+} from '../styledComponent';
+
+
+const myLoader = ({ src, width, quality }) => {
+    return `http://localhost:3000/${src}?w=${width}&q=${quality || 75}`;
+}
+
+const Gif = ({ projectDemoSrc }) => {
+    return (
+        <Image
+            loader={myLoader}
+            src={projectDemoSrc || "assets/demos/Mod1-Project_DEMO-Full_v2"}
+            alt="gif"
+            width={1000}
+            height={1000}
+        />
+    );
+}
 
 const PortfolioCardFront = ({ project }) => {
-  return (
-    <>
-      <CardTitle>
-        {project.title}
-      </CardTitle>
-
-      <CardDescription>
-        {project.desc}
-      </CardDescription>
-
-      <CardLinkWrapper className='portfolio-card-content__icon'>
-        {project.icons.map(icon =>
-          <CardLink
-            key={icon.id}
-            target="_blank"
-            rel="noopener noreferrer"
-            href={icon.link}
-          ><CardIcon className={icon.classname}></CardIcon></CardLink>)
-        }
-      </CardLinkWrapper>
-
-      <CardLinkWrapper className='portfolio-card-content__links'>
-        {project.github.link.length > 1
-          ? project.github.link.map(repo =>
-            <CardLink
-              key={repo.label}
-              target="_blank"
-              rel="noopener noreferrer"
-              href={repo.link}
-            >{repo.label}</CardLink>)
-          : (
-            <CardLink
-              target="_blank"
-              rel="noopener noreferrer"
-              href={project.github.link[0]}
-            >
-              {project.github.label}
-            </CardLink>)
-        }
-      </CardLinkWrapper>
-    </>
-  );
+    return (
+        <>
+            <CardTitle>
+                {project.title}
+            </CardTitle>
+            <Gif projectDemoSrc={project.demo} />
+            <CardDescription>
+                {project.desc}
+            </CardDescription>
+        </>
+    );
 };
 
 
